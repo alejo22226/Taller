56b714247faac44d52adc99ddba376aae3372ef1
Estiven Zapata <estivenzapata99@gmail.com>
2019-09-28 22:38:55 -0500

    Prueba de testing
    Configuración para el funcionamiento de Jest con el fin de hacer las pruebas unitarias

---
+++ __tests__/index.test.js
@@ -0,0 +1,5 @@
+const resta = (a, b) => a - b
+
+test("Example resta", () => {
+    expect(resta(10, 6)).toBe(5)
+});
---
+++ bd/bd.js
@@ -0,0 +1,13 @@
+const { Client } = require('pg')
+
+client = new Client({
+    user: 'postgres',
+    password: 'postgres',
+    port: 5432,
+    database: 'matiz'
+})
+
+
+module.exports = {
+    client
+}
